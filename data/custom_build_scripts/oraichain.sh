f4dd1d1bfc81230037d58f1b497f6092690b6425
phutx <phu.tx@orai.io>
2024-06-18 17:46:03 +0700

    add oraichain.sh to install binary

---
+++ data/custom_build_scripts/oraichain.sh
@@ -0,0 +1,17 @@
+cd $HOME
+
+if [[ -z $upgrading ]]; then
+  git clone --single-branch --branch $version $git_repo
+  repo_name=$(basename $git_repo |cut -d. -f1)
+  cd $repo_name
+else
+  repo_name=$(basename $git_repo |cut -d. -f1)
+  cd $repo_name
+  git reset --hard
+  git fetch --all --tags
+  git checkout "$p_version"
+fi
+cd orai
+go mod tidy
+make install
+echo "finish install go binary"
